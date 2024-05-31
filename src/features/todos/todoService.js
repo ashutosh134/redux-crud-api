cbd7d196c6faaf78b3c8325c3bd41dee47a6f98e
ashutosh134 <vermaashutosh134@gmail.com>
2024-05-31 17:53:54 +0530

    #somechange

--- src/features/todos/todoService.js
+++ src/features/todos/todoService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const fetchTodos = async() => {
-    const response = await axios.get('/api/todo');
+    const response = await axios.get('https://crud-api-mlhz.onrender.com/api/todo');
     console.log(response);
     return response.data;
     
@@ -9,18 +9,18 @@ export const fetchTodos = async() => {
 
 
 export const deleteTodo = async(id) => {
-   const response = await axios.delete("/api/todo/" + id);
+   const response = await axios.delete("https://crud-api-mlhz.onrender.com/api/todo/" + id);
    return response.data;
 };
 
 
 export const saveTodo =async(formData) =>{
-    const response = await axios.post("/api/todo" , formData)
+    const response = await axios.post("https://crud-api-mlhz.onrender.com/api/todo" , formData)
     return response.data;
     
 }
 
 export const update = async(id ,formData)=>{
-const response = await axios.put("/api/todo" + formData.id ,formData)
+const response = await axios.put("https://crud-api-mlhz.onrender.com/api/todo" + formData.id ,formData)
 console.log(response.data)
 }
